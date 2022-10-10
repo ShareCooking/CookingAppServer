5ccb18168e0d654536eb72ca6ea4eb18a80f5aee
selly <selly@192.168.219.100>
2022-10-10 18:10:51 +0900

    회원가입 추가 로그인 기능에 패스워드 암호화 추가

--- src/main/java/com/cooking/vo/UserVO.java
+++ src/main/java/com/cooking/vo/UserVO.java
@@ -7,4 +7,21 @@ public class UserVO {
 	
 	private String user_id;
 	private String user_pw;
+	
+	private String user_nickname;
+	private String user_addr1;
+	private String user_addr2;
+	private String user_addr3;
+	private int user_tel1;
+	private int user_tel2;
+	private int user_tel3;
+	private int user_btd;
+	
+
+	private String user_pw_old1;
+	private String user_pw_old2;
+	private String user_pw_old3;
+	private String user_pw_old4;
+	private String user_pw_old5;
+	
 }
